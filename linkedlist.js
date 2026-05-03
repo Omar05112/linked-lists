d8bbcdee8591ffdc0ddfb6456bfba7b11d7cbabc
Omar05112 <omarhamaini1@gmail.com>
2026-05-03 01:17:16 +0100

    Create all necessary functions.

--- linkedlist.js
+++ linkedlist.js
@@ -54,4 +54,63 @@ export default class LinkedList {
       return tmp;
     }
   }
+
+  at(index) {
+    if (this.heade === null) return undefined;
+
+    let tmp = this.heade;
+    for (let i = 0; i <= index; i++) {
+      if (i === index) break;
+      tmp = tmp.nextNode;
+      if (tmp === null) return undefined;
+    }
+
+    return tmp.value;
+  }
+
+  pop() {
+    if (this.heade === null) return undefined;
+    let prev = this.heade;
+    this.heade = this.heade.nextNode;
+    return prev.value;
+  }
+
+  contains(value) {
+    if (this.heade === null) return undefined;
+
+    let tmp = this.heade;
+    while (tmp !== null && tmp.value !== value) {
+      tmp = tmp.nextNode;
+    }
+    if (tmp === null) return false;
+
+    if (tmp.value == value) return true;
+    else return false;
+  }
+
+  findIndex(value) {
+    if (this.heade === null) return undefined;
+    let i = 0;
+    let tmp = this.heade;
+    while (tmp !== null && tmp.value !== value) {
+      i++;
+      tmp = tmp.nextNode;
+    }
+    if (tmp === null) return -1;
+    else return i;
+  }
+
+  toString() {
+    if (this.heade === null) return "";
+    let string = "";
+
+    let tmp = this.heade;
+    while (tmp !== null) {
+      string += `( ${tmp.value} ) -> `;
+      tmp = tmp.nextNode;
+    }
+    if (tmp === null) string += "null";
+
+    return string;
+  }
 }
